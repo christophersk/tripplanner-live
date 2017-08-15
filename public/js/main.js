d8220a0b1a0d4a8262c3e41b47ad3ebad026e34d
Chris Skene <cnskene@gmail.com>
2017-08-15 17:34:58 -0500

    end of day tuesday

--- public/js/main.js
+++ public/js/main.js
@@ -61,3 +61,137 @@ $(function initializeMap (){
     drawMarker('activity', [41.8675766, -87.6162267])
 
 });
+
+$(document).ready(function() {
+
+  function returnCurrentDay() {
+    return +$('.current-day').text();
+  }
+
+  const itinerary = [null, {}];
+
+  function createMarker(coordinates) {
+    const coordsArray = coordinates.split(',');
+
+    var myLatlng = new google.maps.LatLng(+coordsArray[0],+coordsArray[1]);
+    var mapOptions = {
+      zoom: 15,
+      center: myLatlng
+    }
+    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        title:"Hello World!"
+    });
+        // To add the marker to the map, call setMap();
+        marker.setMap(map);
+  }
+
+  hotels.forEach(hotel => {
+    $('#hotel-choices').append(`<option data-coordinates="${hotel.place.location}">${hotel.name}</option>`);
+  })
+  restaurants.forEach(restaurant => {
+    $('#restaurant-choices').append(`<option>${restaurant.name}</option>`);
+  })
+  activities.forEach(activity => {
+    $('#activity-choices').append(`<option>${activity.name}</option>`);
+  })
+
+  $('#add-hotel').on('click', function() {
+    const hotelName = $('#hotel-choices').val();
+    itinerary[returnCurrentDay()].hotel = [hotelName];
+    createMarker($('#hotel-choices').find(':selected').data('coordinates'));
+
+    $(`#day-${returnCurrentDay()}`).trigger('click');
+  })
+
+  $('#add-restaurant').on('click', function() {
+    const restaurantName = $('#restaurant-choices').val();
+    if (!itinerary[returnCurrentDay()].hasOwnProperty('restaurant')) {
+      itinerary[returnCurrentDay()].restaurant = [restaurantName];
+    } else if (itinerary[returnCurrentDay()].restaurant.indexOf(restaurantName) === -1) {
+      itinerary[returnCurrentDay()].restaurant.push(restaurantName);
+    }
+    $(`#day-${returnCurrentDay()}`).trigger('click');
+  })
+
+  $('#add-activity').on('click', function() {
+    console.log($('#activity-choices').val())
+    const activityName = $('#activity-choices').val();
+    if (!itinerary[returnCurrentDay()].hasOwnProperty('activity')) {
+      itinerary[returnCurrentDay()].activity = [activityName];
+    } else if (itinerary[returnCurrentDay()].activity.indexOf(activityName) === -1) {
+      itinerary[returnCurrentDay()].activity.push(activityName);
+    }
+    $(`#day-${returnCurrentDay()}`).trigger('click');
+  })
+
+  $('.list-group').on('click', '.remove', function(){
+    console.log($(this).data('name'));
+    const category = $(this).data('category');
+    const name = $(this).data('name');
+    const categoryArray = itinerary[returnCurrentDay()][category];
+    const itemIndex = categoryArray.indexOf(name);
+    categoryArray.splice(itemIndex, 1);
+    $(this).parent().remove();
+    console.log(itinerary);
+  })
+
+  $('#day-add').on('click', function() {
+    $(this).before(`<button id="day-${itinerary.length}" class="btn btn-circle day-btn">${itinerary.length}</button>`);
+    itinerary.push({});
+  })
+
+  $('.remove').on('click', function() {
+    const deletedDay = returnCurrentDay()
+    itinerary.splice(deletedDay, 1);
+    const numberOfButtons = $('.day-btn').length - 1;
+    $('.day-btn').each(function(index, elem) {
+      if (index !== numberOfButtons) {
+        $(elem).remove();
+      }
+    })
+    itinerary.forEach((day, index) => {
+      if (index !== 0) {
+        $('#day-add').before(`<button id="day-${index}" class="btn btn-circle day-btn">${index}</button>`);
+      }
+    })
+
+    $(`#day-${deletedDay}`).trigger('click');
+  })
+
+  $('.day-buttons').on('click', '.day-btn', function() {
+    if ($(this)[0].id !== 'day-add') {
+      const dayNumber = +$(this).text();
+      $('.current-day').removeClass('current-day');
+      $(this).addClass('current-day');
+      $('#day-title-name').text(`Day ${dayNumber}`);
+
+      let hotelString = '';
+      if (Array.isArray(itinerary[returnCurrentDay()].hotel)) {
+        hotelString = `<li>${itinerary[returnCurrentDay()].hotel}<span data-category="hotel" data-name="${itinerary[returnCurrentDay()].hotel}" class="glyphicon glyphicon-remove-circle remove"></span></li>`;
+      }
+      $('#hotel-list').html(hotelString);
+
+      let restaurantString = '';
+      if (Array.isArray(itinerary[returnCurrentDay()].restaurant)) {
+        restaurantString = itinerary[returnCurrentDay()].restaurant.reduce((acc, restaurant) => {
+          return acc.concat(`<li>${restaurant}<span data-category="restaurant" data-name="${restaurant}" class="glyphicon glyphicon-remove-circle remove"></span></li>`)
+        }, '')
+      }
+      $('#restaurant-list').html(restaurantString);
+
+
+      let activityString = '';
+      if (Array.isArray(itinerary[returnCurrentDay()].activity)) {
+        activityString = itinerary[returnCurrentDay()].activity.reduce((acc, activity) => {
+          return acc.concat(`<li>${activity}<span data-category="activity" data-name="${activity}" class="glyphicon glyphicon-remove-circle remove"></span></li>`)
+        }, '')
+      }
+      $('#activity-list').html(activityString);
+
+    }
+  })
+
+})
